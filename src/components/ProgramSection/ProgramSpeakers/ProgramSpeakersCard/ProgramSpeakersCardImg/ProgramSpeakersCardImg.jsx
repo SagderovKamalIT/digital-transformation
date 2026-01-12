4eb2caedf548ab52507d36b5cc6b50e634a70026
Kamal Sagderov <sagderovkamal.it@gmail.com>
2026-01-12 16:43:35 +0300

    Updates

--- src/components/ProgramSection/ProgramSpeakers/ProgramSpeakersCard/ProgramSpeakersCardImg/ProgramSpeakersCardImg.jsx
+++
@@ -1,9 +0,0 @@
-import styles from "./ProgramSpeakersCardImg.module.scss"
-
-const ProgramSpeakersCardImg = ({ src, alt }) => {
-  return (
-    <img className={styles.speakers__img} src={src} alt={alt} />
-  )
-}
-
-export default ProgramSpeakersCardImg;
--- src/components/ProgramSection/ProgramSpeakers/ProgramSpeakersTitle/ProgramSpeakersTitle.jsx
+++
@@ -1,8 +0,0 @@
-import styles from "./ProgramSpeakersTitle.module.scss";
-
-const ProgramSpeakersTitle = ({ children }) => {
-  return <h4 className={styles.speakers__title}>{children}</h4>;
-};
-
-export default ProgramSpeakersTitle;
-
